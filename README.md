17c2400b216fe754bdba68a299ab8ceb0df648d7
seoyoung <1203sylim@gmail.com>
2025-03-17 15:00:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# gitignore 
+
+
+# 가상환경 설정
+
+
+## 1. Django  프로젝트
+
+
+1. Django 설치
+```shell
+pip install django
+```
+
+2. 프로젝트 생성
+```
+django-admin startproject <pjt-name> <path>
+```
+
+3. 서버 실행
+서버 종료 >> ctrl + c
+```
+python manage.py runserver
+
+4. 앱 생성
+```shell
+django-admin startapp <app.name>
+```
+
+5. 앱 등록('settings.py')
+```python
+INSTALLED_APPS = [
+
+]
+```
+
+MTV(templates, models, views)
+client > urls.py(어떤 경로로 받을지 -> path) > Views.py(실행시키고 싶은 함수 함수 이름은 경로와 맞추기. 선언과 불러오는 것은 각각) > models.py > template(HTML) 데이터 수정 > view.py >> (사이클 반복) >>> (최종) response(HTML)
+
+
+{% for fake_article in fake_articles %}
+        <p>{{fake_article}}</p>
+    {% endfor %}
+    >> 잔고에서 파이썬 코드 활용법
+
+    . >> class (div 활용)
+    # >> id
---
+++ first_app/views.py
@@ -0,0 +1,70 @@
+import random
+from faker import Faker
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def hello(request):
+    my_name = 'seoyoung'
+
+    context = {
+        'my_name': my_name,
+    }
+
+    return render(request, 'hello.html', context)
+
+def lunch(request):
+    menu = ['김밥', '초밥', '돈까스', '우동']
+    
+    pick = random.choice(menu)
+
+    context = {
+        'pick': pick,
+    }
+
+    return render(request, 'lunch.html', context)
+
+def lotto(request):
+    lucky_number = random.sample(range(1, 46), 6)
+
+    # number = random.choice(lucky_number)
+
+    context = {
+        'lucky_number': lucky_number,
+    }
+
+
+    return render(request, 'lotto.html', context)
+
+
+def profile(request, username):
+    context = {
+        'username': username,
+    }
+
+    return render(request, 'profile.html', context)
+
+def cube(request, number):
+    result = number ** 3
+
+    context = {
+        'number': number,
+        'result': result,
+    }
+
+    return render(request, 'cube.html', context)
+
+def articles(request):
+    fake = Faker()
+    fake_articles = []
+
+    for i in range(100):
+        fake_articles.append(fake.text())
+
+    context = {
+        'fake_articles': fake_articles,
+    }
+
+    return render(request, 'articles.html', context)
